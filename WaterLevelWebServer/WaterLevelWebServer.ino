728243f98737fae30224970ccfa593b6f71c67d4
Shantnav Agarwal <shantnavagarwal@gmail.com>
2023-12-02 13:55:13 +0100

    Added sensing for temprature and humidity. Wrote code for data logging but it has bugs and is not working

--- WaterLevelWebServer/WaterLevelWebServer.ino
+++ WaterLevelWebServer/WaterLevelWebServer.ino
@@ -9,12 +9,18 @@
 #include <AsyncTCP.h>
 #include "ESPAsyncWebSrv.h"
 #include "esp_system.h"
+#include "FS.h"
+#include "SPIFFS.h"
+#include <DFRobot_DHT20.h>
+
 
 // Shared Memory
-volatile int water_level = 0; // In centimeter
+volatile double water_level = 0; // In centimeter
 volatile bool mode_automatic = true;
 volatile bool pump_status = false;
-volatile bool target_water_level = 0;
+volatile int target_water_level = 0;
+volatile double temperature_c = 0;
+volatile double humidity_p = 0;
 
 // Define a semaphore for synchronization
 SemaphoreHandle_t xSemaphore;
@@ -35,9 +41,49 @@ AsyncWebServer server(80);
 const int TRIG_PIN = 27;
 const int ECHO_PIN = 14;
 unsigned long duration = 0;
-int distance_ultrasonic = 0; // In Centimeter
+double distance_ultrasonic = 0; // In Centimeter
 const int TimeDelay = 500; // milliseconds -> 500 == 2 Hz
 const int container_depth = 20; // In centimeter
+
+// Temprature and Humidity sensor setup
+/*!
+ * @param pWire IC bus pointer object and construction device, can both pass or not pass parameters, Wire in default. SCL - GPIO 22, SDA - GPIO 21
+ * @param address Chip IIC address, 0x38 in default.
+ */
+DFRobot_DHT20 dht20;
+
+// Logging Setup
+String logdata_path = "/data.csv";
+
+void logData(String target_water_level_, String water_level_, String pump_status_, String humidity_, String temperature_){
+  if (!SPIFFS.begin()) {
+    Serial.println("Failed to mount file system");
+    return;
+  }
+
+  // Open the CSV file for appending
+  File file = SPIFFS.open(logdata_path, "a");
+  if (!file) {
+    Serial.println("Failed to open file for appending\nCreating new file: " + logdata_path);
+    file = SPIFFS.open(logdata_path, "w");
+    if (!file){
+      Serial.println("Could not create file. Exiting ...");
+      return;
+    }
+    Serial.println("File created");
+    file.println("Target water level,Water Level,Pump Status,Humidity%,Temprature(C)");
+  }
+  Serial.println("File Opened");
+
+    // Append the data to the CSV file
+  file.println(target_water_level_ + "," + water_level_ + "," + pump_status_ + "," + humidity_ + "," + temperature_);
+  
+
+  // Close the file
+  file.close();
+
+  Serial.println("Data appended to data.csv");
+}
 
 void sendHtml(AsyncWebServerRequest *request) {
   Serial.println("Webpage Served");
@@ -62,10 +108,13 @@ void sendHtml(AsyncWebServerRequest *request) {
       <body>
         <h1>Acraea Control Interface</h1>
         <div>
+          <h3>Temprature: TEMPRATURE_INP C</h3>
+          <h3>Humidity: HUMIDITY_INP % </h3>
           <h3>Water Level: WATER_LEVEL_INP cm</h3>
           <h3>Pump Status: PUMP_STATUS_INP</h3>
           <a href="/toggle/pump_mode" class="btn pump_mode">Switch to PUMP_MODE_INP mode</a>
           PUMP_MODE_SPECIFIC
+          <a href="/download_data" class="btn download_data">Download logged data</a>
         </div>
       </body>
     </html>
@@ -84,7 +133,9 @@ void sendHtml(AsyncWebServerRequest *request) {
 // <a href="/toggle/2" class="btn LED2_TEXT">LED2_TEXT</a>
   if (xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
     // Access and modify the shared variable
-    response.replace("WATER_LEVEL_INP", String(water_level));
+    response.replace("TEMPRATURE_INP", String(temperature_c, 2));
+    response.replace("HUMIDITY_INP", String(humidity_p, 2));
+    response.replace("WATER_LEVEL_INP", String(water_level, 2));
     response.replace("PUMP_STATUS_INP", pump_status ? "ON" : "OFF");
     response.replace("PUMP_MODE_INP", mode_automatic ? "Manual" : "Automatic");
     if (mode_automatic){
@@ -145,17 +196,34 @@ void setWaterLevel(AsyncWebServerRequest *request){
     request->redirect("/");
   }
 
+void downloadLoggedData(AsyncWebServerRequest *request){
+  if (xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
+    File file = SPIFFS.open(logdata_path, "r");
+    if (!file) {
+      request->send(404, "text/plain", "File not found");
+    }
+    else{
+      request->send(SPIFFS, logdata_path, "application/octet-stream");
+      // Send the file as a response
+      file.close();
+    }
+    xSemaphoreGive(xSemaphore);
+  }
+  return;
+}
+
 void core1Task(void *parameters){
+  // Run the webserver
   server.on("/", HTTP_GET, sendHtml);
   server.on("/toggle/pump_mode", HTTP_GET, changePumpMode);
   server.on("/mode_automatic_form", HTTP_GET, setWaterLevel);
   server.on("/toggle/pump_status", HTTP_GET, changePumpStatus);
+  server.on("/download_data", HTTP_GET, downloadLoggedData);
   server.onNotFound(notFound);
   server.begin();
   Serial.println("HTTP server started");
 
   while(true) {
-    // server.handleClient();
     delay(2);
   }
 }
@@ -167,21 +235,27 @@ void sense_ultrasonic(){
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
   const unsigned long duration= pulseIn(ECHO_PIN, HIGH);
-  distance_ultrasonic = duration/29/2;
+  distance_ultrasonic = double(duration)/29.0/2.0;
 }
 
-// void sense_ultrasonic(){
-//   distance_ultrasonic = random(5, 15);
-// }
+void sense_temp_humi(){
+  if (xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
+    temperature_c = dht20.getTemperature();
+    humidity_p = dht20.getHumidity()*100.0;
+    // Release the semaphore
+    xSemaphoreGive(xSemaphore);
+  }
+}
 
 void control_algorithm(){
   if (xSemaphoreTake(xSemaphore, portMAX_DELAY)) {
     // Access and modify the shared variable
-    water_level = max(container_depth - distance_ultrasonic, 0);
+    water_level = max(double(container_depth) - distance_ultrasonic, 0.0);
     if (mode_automatic){
       pump_status = water_level < target_water_level ? true : false;
     }
-    digitalWrite(pump_control_signal, pump_status ? 0 : 255);
+    digitalWrite(pump_control_signal, pump_status ? 255 : 0);
+    logData(String(target_water_level), String(water_level, 2), String(pump_status), String(humidity_p, 2), String(temperature_c, 2));
     // Release the semaphore
     xSemaphoreGive(xSemaphore);
   }
@@ -189,9 +263,11 @@ void control_algorithm(){
 }
 
 void core2Task(void *parameters){
+  // Read data from all sensors and store in shared memory space.
   while (true){
     sense_ultrasonic();
     control_algorithm();
+    sense_temp_humi();
     delay(TimeDelay);
   }
 }
@@ -213,8 +289,14 @@ void setup(void) {
 
   Serial.print("IP address: ");
   Serial.println(WiFi.softAPIP());
+  Serial.println("Connecting to Temprature and humidity sensor ...");
+  while(dht20.begin()){
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("Connected");
 
-    xTaskCreatePinnedToCore(
+  xTaskCreatePinnedToCore(
     core1Task,  // Task function
     "Core 1 Task",  // Task name
     10000,  // Stack size
